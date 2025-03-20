c56cc7c82ff2f87779b9382d8222a510e7ae3118
Claudiu1503 <adichis1503@yahoo.com>
2025-03-20 18:56:21 +0200

    First push - empty image

---
+++ src/main/java/net/arhitecture/filmeflixlogin/FilmeflixLoginApplication.java
@@ -0,0 +1,13 @@
+package net.arhitecture.filmeflixlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeflixLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmeflixLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/arhitecture/filmeflixlogin/FilmeflixLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arhitecture.filmeflixlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeflixLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
